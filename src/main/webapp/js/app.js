c1a71e3b13b12bad1d6804af0b6fcf88cb954e2e
Anglain <shudra.ia@gmail.com>
2018-02-28 11:17:31 +0200

    Changes in apis, practice done

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -86,7 +86,7 @@ app.constant('HTTP_ERRORS', {
  */
 app.factory('oauth2Provider', function ($modal) {
     var oauth2Provider = {
-        CLIENT_ID: 'replace with your client id',
+        CLIENT_ID: '846662647498-9b4qei2gpuaqe8p8buvrsk4n98adma3l.apps.googleusercontent.com',
         SCOPES: 'https://www.googleapis.com/auth/userinfo.email profile',
         signedIn: false
     };
